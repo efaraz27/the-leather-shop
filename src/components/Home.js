d11ba76d1e653cde03f60b90e759730ab64b8640
Djas <djas@gmail.com>
2021-08-03 01:30:59 +0530

    app is now responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,58 @@
 import React from 'react'
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+
 import "./css/Home.css"
 
+//image imports
+import p1 from '../assets/1.jpg'
+import p2 from '../assets/2.jpg'
+import p3 from '../assets/3.jpg'
+import p4 from '../assets/4.jpg'
+
+
+
 const Home = () => {
+    let slides = [{
+        image: p1,
+        title: 'Shoes',
+        description: 'Premium Leather Shoes',
+        button: `Buy Shoes`,
+    }, {
+        image: p2,
+        title: 'Handmade Leather',
+        description: '100% Hand Crafted Premium Leather',
+        button: 'Read More',
+    },{
+        image: p3,
+        title: 'Skilled Professionals',
+        description: 'Experienced and Skilled Professionals',
+        button: 'Read More',
+    },{
+        image: p4,
+        title: 'Premium Tools',
+        description: 'We use premium tools to craft premium products',
+        button: 'Read More',
+    }]
     return(
         <div className='Home'>
-            <div className='slogan'>Premium Leather Products</div>
-            <div className='sub-slogan'>100% Hand Made Leather</div>
+            <div className='wrapper'>
+                <Slider className="slider-wrapper">
+                {slides.map((item, index) => (
+                    <div
+                        key={index}
+                        className="slider-content"
+                        style={{ background: `url('${item.image}') no-repeat center center` }}
+                    >
+                        <div className="inner">
+                            <h1>{item.title}</h1>
+                            <p>{item.description}</p>
+                            <button>{item.button}</button>
+                        </div>
+                    </div>
+                ))}
+                </Slider>
+            </div>
         </div>
     )
 }
--- src/components/NavbarMaterial.js
+++ src/components/NavbarMaterial.js
@@ -4,9 +4,8 @@ import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ProductsBtn from "./ProductsBtn";
+import Drawer from './Drawer'
 import './css/NavbarMaterial.css'
 
 export default function ButtonAppBar() {
@@ -14,15 +13,7 @@ export default function ButtonAppBar() {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" style={{backgroundColor:'#303030'}}>
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
+                    <div className='drawer'> <Drawer/> </div>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         The Leather Shop
                     </Typography>
